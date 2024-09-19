436b9e487554fea4691b581b1f03a740e8c1d5dd
Seungsik Bae <0730bss@gmail.com>
2024-09-20 00:20:31 +0900

    Grafeat: Grammarly 도메인 한정 검색 기능 구현

--- script_editor/user_feedback/custom-search-test.py
+++ script_editor/user_feedback/custom-search-test.py
@@ -7,16 +7,21 @@ dotenv.load_dotenv() # .env에서 환경 변수 로드하기
 
 SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
 SEARCH_ENGINE_API_KEY = os.getenv('SEARCH_ENGINE_API_KEY')
-search_query = "subject and verb agreement"
+search_query = "past tense"  # 쿼리 개선 기능으로 보완해도 됨.
 CUSTOM_SEARCH_ENGINE_URI = f"https://www.googleapis.com/customsearch/v1?key={SEARCH_ENGINE_API_KEY}&cx={SEARCH_ENGINE_ID}&q={search_query}"
 
-#response = requests.get(CUSTOM_SEARCH_ENGINE_URI).json() // 검색 결과 모두 받아오기
+data = requests.get(CUSTOM_SEARCH_ENGINE_URI).json() # 검색 결과 모두 받아오기
 #print(response)
 
-f = open('data.json')
-search_result = json.load(f)
+# Test JSON file
+#f = open('data.json')
+#data = json.load(f)
 
-print(search_result['items'])
+top10_search_results = []
 
-for i in search_result['items']:
-    print(i['title'], i['link'])
+for i in range(len(data['items'])):
+    title = data['items'][i]['title']
+    link = data['items'][i]['link']
+    top10_search_results.append({"result_index": i, "title": title, "link": link})
+
+print(top10_search_results)
