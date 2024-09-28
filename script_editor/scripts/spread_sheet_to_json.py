a48e2b87a1a412b61be3b04446580b7d73937895
Seungsik Bae <0730bss@gmail.com>
2024-09-28 18:10:56 +0900

    feature: json 파일을 spread sheet에 업데이트하는 기능 추가

--- script_editor/scripts/spread_sheet_to_json.py
+++ script_editor/scripts/spread_sheet_to_json.py
@@ -8,15 +8,9 @@ def camel_case(s):
     return camel_cased
 
 
-def create_sheety_end_point(tag_):
-    tag_ = camel_case(tag_)
-    sheety_end_point = f"https://api.sheety.co/ecdda9522fa86a7eb2b459b27d6bec30/lhcWrongSentenceData/{tag_}"
-    return sheety_end_point
+GRAMMAR_DATA = []
 
-
-grammar_data = []
-
-sheet_tags = {
+SHEET_TAGS = {
     "Subject-Verb Agreement": "주어와 동사의 수일치 문제",
     "Article Usage Errors": "관사 사용 오류",
     "Preposition Usage Errors": "전치사 사용 오류",
@@ -25,27 +19,40 @@ sheet_tags = {
     "Capitalization Errors": "대문자 사용 오류",
 }
 
-for tag, explanation in sheet_tags.items():
-    sheet_data = requests.get(create_sheety_end_point(tag)).json()[
-        camel_case(tag)]  # tag별 틀린 문장, 수정된 문장의 List. List의 각 Element는 dict.
-    data_per_error_type = {
-        "tag": tag,
-        "설명": explanation,
-        "오류_예문_목록": []
-    }
 
-    for i in range(len(sheet_data)):
-        sentence_data = {
-            "문장": sheet_data[i]['문장'],
-            "수정": sheet_data[i]['수정'],
-        }
-        data_per_error_type['오류_예문_목록'].append(sentence_data)
+class WrongSentenceJson:
+    def __init__(self):
+        self.sheet_data = []
+        self.data_per_error_type = []
 
-    grammar_data.append(data_per_error_type)
+    def create_sheety_end_point(self, tag_):
+        tag_ = camel_case(tag_)
+        sheety_end_point = f"https://api.sheety.co/ecdda9522fa86a7eb2b459b27d6bec30/lhcWrongSentenceData/{tag_}"
+        return sheety_end_point
 
-    # print(f"{tag}: {explanation}")
-    # print(sheet_data)
-    # print(grammar_data)
+    def create_json(self):
+        for tag, explanation in SHEET_TAGS.items():
+            sheet_data = requests.get(self.create_sheety_end_point(tag)).json()[
+                camel_case(tag)]  # tag별 틀린 문장, 수정된 문장의 List. List의 각 Element는 dict.
+            data_per_error_type = {
+                "tag": tag,
+                "설명": explanation,
+                "오류_예문_목록": []
+            }
 
-with open("grammar_data.json", "w", encoding="utf-8") as f:
-    json.dump(grammar_data, f, ensure_ascii=False, indent=4)
+            for i in range(len(sheet_data)):
+                sentence_data = {
+                    "문장": sheet_data[i]['문장'],
+                    "수정": sheet_data[i]['수정'],
+                }
+                data_per_error_type['오류_예문_목록'].append(sentence_data)
+
+            GRAMMAR_DATA.append(data_per_error_type)
+
+        with open("grammar_data.json", "w", encoding="utf-8") as f:
+            json.dump(GRAMMAR_DATA, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    inst = WrongSentenceJson()
+    inst.create_json()
---
+++ script_editor/scripts/update_spread_sheet.py
@@ -0,0 +1,67 @@
+import requests
+import json
+
+
+def camel_case(s):
+    words = s.replace("-", " ").split()
+    camel_cased = words[0].lower() + ''.join(word.capitalize() for word in words[1:])
+    return camel_cased
+
+
+# 전역 변수
+GRAMMAR_DATA = []
+
+SHEET_TAGS = {
+    "Subject-Verb Agreement": "주어와 동사의 수일치 문제",
+    "Article Usage Errors": "관사 사용 오류",
+    "Preposition Usage Errors": "전치사 사용 오류",
+    "Tense Errors": "시제 오류",
+    "Pluralization Errors": "복수형 사용 오류",
+    "Capitalization Errors": "대문자 사용 오류",
+}
+
+# JSON 파일 읽기
+with open("grammar_data.json", "r", encoding='utf-8') as f:
+    grammar_data = json.load(f)
+
+
+class WrongSentenceJson:
+
+    def create_sheety_end_point(self, tag_):
+        tag_ = camel_case(tag_)
+        sheety_end_point = f"https://api.sheety.co/ecdda9522fa86a7eb2b459b27d6bec30/lhcWrongSentenceData/{tag_}"
+        return sheety_end_point
+
+    def update_sheet(self):
+        for data in grammar_data:
+            print(data)
+            tag = camel_case(data['tag'])
+            if tag[len(tag)-1] == "s":
+                tag = tag[:len(tag) - 1]
+
+            for sentence in data["오류_예문_목록"]:
+
+                input_data = {
+                    tag: {
+                        "문장": sentence["문장"],
+                        "수정": sentence["수정"],
+                    }
+                }
+
+                # API POST 요청
+                try:
+                    post_response = requests.post(url=self.create_sheety_end_point(data['tag']), json=input_data)
+
+                    # 응답 상태 확인
+                    if post_response.status_code == 200:
+                        print(f"Updated successfully for tag: {data['tag']}")
+                    else:
+                        print(f"Failed to update for tag: {data['tag']} - Status Code: {post_response.status_code}")
+                        print(f"Response: {post_response.json()}")  # 실패 시 응답 내용 출력
+                except Exception as e:
+                    print(f"An error occurred while updating tag: {data['tag']} - Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    inst = WrongSentenceJson()
+    inst.update_sheet()
